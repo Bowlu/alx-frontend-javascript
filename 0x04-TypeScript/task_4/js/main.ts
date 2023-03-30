b77eb2eb2ffdd4014e26b5859f57659ff33cc86d
Awoyemi Boluwatife <awoyemiboluwatife@gmail.com>
2023-03-30 19:21:23 +0000

    update

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -8,8 +8,8 @@ export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: "Bobby",
-  lastName: "Wine",
+  firstName: "John",
+  lastName: "Doe",
   experienceTeachingC: 10,
 };
 
