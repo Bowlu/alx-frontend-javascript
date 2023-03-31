b52173b9732fd107c452b1b23a4b9a51c5595a03
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2023-03-31 22:41:30 +0100

    adjusted square bracket

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,4 +2,5 @@ export default function getStudentIdsSum(array) {
   if(array instanceOf Array) {
     const sumOfStudentId = array.reduce((sum, value) => sum + value.id, 0);
     return sumOfStudenId;
+  }
 }
